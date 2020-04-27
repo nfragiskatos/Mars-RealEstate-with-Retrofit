919481872e75a08d5340265c8cfd8206272a6dba
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-27 15:49:44 -0700

    - Added a 'property' property to the ViewModel and changed the getMarsRealEstateProperties function to arbitrarily set the 'property' value to the first result from our fetched list of MarsProperty objects.
    - In the layout file, the text view will display the image source url using the ViewModel binding.

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApiService
+import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.*
 
 /**
@@ -29,11 +30,17 @@ import kotlinx.coroutines.*
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
     val response: LiveData<String>
-        get() = _response
+        get() = _status
+
+    // MarsProperty to be loaded (just a single item for now.
+    private val _property = MutableLiveData<MarsProperty>()
+
+    val property: LiveData<MarsProperty>
+        get() = _property
 
     /**
      * Need a job and scope now since we are working with Coroutines
@@ -60,10 +67,12 @@ class OverviewViewModel : ViewModel() {
             try
             {
                 var listResult = getPropertiesDeferred.await()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved!"
+                if (listResult.size > 0) {
+                    _property.value = listResult[0]
+                }
             } catch (e: Exception)
             {
-                _response.value = "Failure: ${e.message}"
+                _status.value = "Failure: ${e.message}"
             }
         }
     }
